90d1680dea143b79168f0ed481d3d3050242fbba
Sara Ximena Ortiz Reyes <sxortiz@ucompensar.edu.co>
2024-02-13 16:17:52 -0500

    Geneneric repository

---
+++ Productos.Infrastructure/Adapters/GenericRepository.cs
@@ -0,0 +1,36 @@
+﻿using Products.Domain.Entities;
+using Products.Infrastructure.Ports;
+using Products.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Products.Infrastructure.Adapters;
+
+public class GenericRepository<T> : IRepository<T> where T : DomainEntity
+{
+    readonly Context.Context  _context;
+    readonly DbSet<T> _dbSet;
+
+    public GenericRepository(Context.Context context, DbSet<T> dbSet)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(Context));
+        _dbSet = _context.Set<T>();
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        _ = entity ?? throw new ArgumentNullException(nameof(entity));
+        await _dbSet.AddAsync(entity);
+        return entity;
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id) ?? throw new ArgumentNullException(nameof(id));
+    }
+
+    public void UpdateAsync(T entity)
+    {
+        _dbSet.Update(entity);
+    }
+}
+
---
+++ Productos.Infrastructure/Ports/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using Products.Domain.Entities;
+
+namespace Products.Infrastructure.Ports;
+
+    public interface IRepository<T> where T : DomainEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        void UpdateAsync(T entity);
+
+    }
+
