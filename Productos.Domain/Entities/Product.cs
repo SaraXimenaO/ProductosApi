3c9d00c6860ebe5f1bb96502a2b840d3e253ffcf
Sara Ximena Ortiz Reyes <sxortiz@ucompensar.edu.co>
2024-02-14 10:21:14 -0500

    Entidades y enumeraciones

--- Productos.Domain/Entities/Product.cs
+++ Productos.Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Products.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,20 @@ namespace Products.Domain.Entities
 {
     public class Product : DomainEntity
     {
+
+        public Product(string name, string description, Status status, int stock, decimal price ) 
+        {
+            Name = name;
+            Description = description;
+            Status = status;
+            Stock = stock;
+            Price = price;
+        }
+
+
         public string Name { get; set; }
         public string Description { get; set; }
-        public int Status { get; set; }
+        public Status Status { get; set; }
         public int Stock { get; set; }
         public decimal Price { get; set; }
     }
